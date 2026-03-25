3d738a6dcd8949d7eb17e198f66f6114a0c0313b
Jorgen Lindstrom <jorgen.lindstrom@ductus.se>
2026-03-25 16:06:23 +0100

    refactor: change util/mockDataService

--- utils/mockDataService.ts
+++ utils/mockDataService.ts
@@ -1,30 +1,29 @@
 export function getMockBeslutsdata(handlaggningId: string) {
-    return {
-        handlaggning_id: handlaggningId,
-        kund: {
-            fornamn: "Lisa",
-            efternamn: "Tass",
-            kon: "KVINNA",
-            anstallning: {
-                organisationsnamn: "Mock AB",
-                arbetstid_procent: 100,
-                lon: {
-                    lonesumma: 40000,
-                }
-            }
+  return {
+    handlaggning_id: handlaggningId,
+    kund: {
+      fornamn: "Lisa",
+      efternamn: "Tass",
+      kon: "KVINNA",
+      anstallning: {
+        organisationsnamn: "Mock AB",
+        arbetstid_procent: 100,
+        lon: {
+          lonesumma: 40000,
         },
-        ersattning: [
-            {
-                ersattning_id: `ers-${handlaggningId}-1`,
-                ersattningstyp: "HUNDBIDRAG",
-                omfattning_procent: 100,
-                belopp: 40000,
-                berakningsgrund: 40000,
-                beslutsutfall: "FU",
-                avslagsanledning: "",
-                from: "2025-01-10T00:00:00Z",
-                tom: "2025-01-10T23:59:59Z",
-            }
-        ]
-    };
+      },
+    },
+    ersattning: [
+      {
+        ersattning_id: `item-${handlaggningId}-1`,
+        ersattningstyp: "HUNDBIDRAG",
+        omfattning_procent: 100,
+        belopp: 40000,
+        berakningsgrund: 40000,
+        beslutsutfall: "FU",
+        from: "2025-01-10",
+        tom: "2025-01-10",
+      },
+    ],
+  };
 }
